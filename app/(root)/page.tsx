f16f36e0140ab0ae7ac35a79a155b065fbe077c7
Ritik <jainritik135@gmail.com>
2024-11-12 16:55:21 +0530

    new file and bug fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Searchbar from "@/components/Searchbar";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const post = [
+    {
+      title: "Image Genrator",
+      createdAt: new Date(),
+      views: 50,
+      author: { id: 1, name: "Ritik" },
+      postId: 10,
+      description: "this is post description",
+      image:
+        "https://th.bing.com/th/id/OIP.04mIQUq2nUvN7JGWc5hRywHaEU?w=274&h=180&c=7&r=0&o=5&pid=1.7",
+      category: "Ai",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -20,6 +35,22 @@ export default async function Home({
         </p>
 
         <Searchbar query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : `All Startups Post`}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {post?.length > 0 ? (
+            post.map((post: StartupCardType) => (
+              <StartupCard key={post?.postId} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startup Idea or Pitch Found.</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,74 @@
+import { formateDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const StartupCard = ({ post }: { post: StartupCardType }) => {
+  const {
+    title,
+    createdAt,
+    views,
+    author: { id: authorId, name },
+    postId,
+    description,
+    image,
+    category,
+  } = post;
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formateDate(createdAt)}</p>
+
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+
+          <Link href={`/startup/${postId}`}>
+            <p className="text-16-medium line-clamp-1">{title}</p>
+          </Link>
+        </div>
+
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src="/Apple.png"
+            alt="user avatar"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`/startup/${postId}`}>
+        <p className="startup-card_desc">{description}</p>
+
+        <Image
+          src={image}
+          alt="post image"
+          className="startup-card_img"
+          width={100}
+          height={100}
+        />
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query/${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${postId}`}>View More</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default StartupCard;
